4188f254eb8e212bb7cd90ef8c87a7110513e45b
Vuttur <bhavana.vuttur@capgemini.com>
2021-06-30 17:38:41 +0530

    formatted code

--- test/specs/formpagetest.js
+++ test/specs/formpagetest.js
@@ -1,28 +1,17 @@
-
-const formpage= require("../pageobjects/form.page")
-
+const formpage = require("../pageobjects/form.page")
 
 describe('Validating title page', () => {
     it('validate the title', async () => {
         await formpage.openBrowser();
-       expect(await formpage.homepage_Logo).toBeDisplayed();
-       
-        
+        expect(await formpage.homepage_Logo).toBeDisplayed();
     });
-     
-     it('Navigating to formpage' , async () => {
-          
-            await formpage.openForm();
 
-    
-  });
- 
+    it('Navigating to formpage', async () => {
+        await formpage.openForm();
+    });
+
     it('Entering details', async () => {
-    await formpage.formData('Bhavana','vuttur','bhavana23@gmail.com','Argentina (+54)','9876543212','NZB','jkpt','TS','506000',"22/03/1990")
-
+        await formpage.formData('Bhavana', 'vuttur', 'bhavana23@gmail.com', 'Argentina (+54)', '9876543212', 'NZB', 'jkpt', 'TS', '506000', "22/03/1990")
 
     });
-    
- 
-    
 });
